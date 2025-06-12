eb95907b439eb793fc47623ab129f300bfed8a6a
Kaskyjames <izuchukwujameskasky@gmail.com>
2025-06-12 23:20:06 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwc9_nkfJUXa4cklK-05PpN4VK3VdWB5o",
+  authDomain: "ai-trading-app-235d0.firebaseapp.com",
+  projectId: "ai-trading-app-235d0",
+  storageBucket: "ai-trading-app-235d0.firebasestorage.app",
+  messagingSenderId: "1035788471781",
+  appId: "1:1035788471781:web:5dc56ee8d9334dae74730c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
